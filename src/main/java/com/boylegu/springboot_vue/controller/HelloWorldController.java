1c80c14bf95648e9a06867ecc9252357c165e27b
顾鲍尔 <gubaoer@QXIT-PC-000087.local>
2017-06-21 21:17:55 +0800

    # init proj

---
+++ src/main/java/com/boylegu/springboot_vue/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.boylegu.springboot_vue.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String index() {
+
+        return "Hello World xx";
+    }
+}
---
+++ src/test/java/com/boylegu/springboot_vue/SpringbootVueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.boylegu.springboot_vue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootVueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
