dc58d413eac30b6e6211c3bb6265b968749bf92c
顾鲍尔 <gubaoer@QXIT-PC-000087.lan>
2017-06-24 20:31:03 +0800

    close #8 finished sex api and override it response formatting

--- src/main/java/com/boylegu/springboot_vue/dao/PersonsRepository.java
+++ src/main/java/com/boylegu/springboot_vue/dao/PersonsRepository.java
@@ -1,9 +1,17 @@
 package com.boylegu.springboot_vue.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.boylegu.springboot_vue.entities.Persons;
 
+import java.util.List;
+
 
 public interface PersonsRepository extends JpaRepository<Persons, Long> {
+
+    public static final String FIND_SEX = "select DISTINCT sex from Persons p";
+
+    @Query(FIND_SEX)
+    List<Persons> findSex();
 }
