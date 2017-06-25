cf8b17cb0a85890ae14c12e5964ff19908d914bf
顾鲍尔 <gubaoer@QXIT-PC-000087.lan>
2017-06-25 19:15:57 +0800

    close #11 support CORS

--- src/main/java/com/boylegu/springboot_vue/App.java
+++ src/main/java/com/boylegu/springboot_vue/App.java
@@ -3,6 +3,7 @@ package com.boylegu.springboot_vue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class App {
 
