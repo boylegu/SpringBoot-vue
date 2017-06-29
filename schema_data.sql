70fe97c4d39a9b670d7f88ac24027776d5ef3cba
顾鲍尔 <gubaoer@QXIT-PC-000087.local>
2017-06-29 13:41:51 +0800

    close #14 handle timestamp problem

--- schema_data.sql
+++ schema_data.sql
@@ -3,17 +3,19 @@
   */
 
 
-CREATE TABLE persons (id  integer, create_datetime datetime, email varchar(255), phone varchar(255), sex varchar(255), username varchar(255), zone blob, primary key (id));
+/* CREATE TABLE persons (id  integer, create_datetime datetime, email varchar(255), phone varchar(255), sex varchar(255), username varchar(255), zone blob, primary key (id));
+*/
+
 
 INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'gubaoer@hotmail.com', 08613000001111, 'male', 'gubaoer', 'HongKou District');
 
 INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'boyle.gu@hotmail.com', 08613000001112, 'male', 'baoer.gu', 'JingAn District');
 
-INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'yoyo.wu@gmail.com', 08613000001113, 'fmale', 'yoyo.wu', 'JingAn District');
+INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'yoyo.wu@gmail.com', 08613000001113, 'female', 'yoyo.wu', 'JingAn District');
 
-INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'stacy.gao@126.com', 08613000001114, 'fmale', 'stacy.gao', 'MinHang District');
+INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'stacy.gao@126.com', 08613000001114, 'female', 'stacy.gao', 'MinHang District');
 
-INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'yomiko.zhu@qq.com', 08613000001115, 'fmale', 'yomiko.zhu', 'PuDong District');
+INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'yomiko.zhu@qq.com', 08613000001115, 'female', 'yomiko.zhu', 'PuDong District');
 
 INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'hong.zhu@163.com', 08613000001116, 'male', 'hong.zhu', 'YangPu District');
 
@@ -23,12 +25,12 @@ INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES 
 
 INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'wen.liu@360.com', 08613000001119, 'male', 'wen.liu', 'ChongMing District');
 
-INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'cai.lu@baidu.com', 08613000001120, 'fmale', 'cai.lu', 'BaoShan District');
+INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'cai.lu@baidu.com', 08613000001120, 'female', 'cai.lu', 'BaoShan District');
 
 INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'alex.li@icee.com', 08613000001121, 'male', 'alex.li', 'ChangNing District');
 
-INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'sofia.xu@qq.com', 08613000001122, 'fmale', 'sofia.xu', 'ZhaBei District');
+INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'sofia.xu@qq.com', 08613000001122, 'female', 'sofia.xu', 'ZhaBei District');
 
-INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'cora.zhang@qq.com', 08613000001123, 'fmale', 'cora.zhang', 'XuHui District');
+INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'cora.zhang@qq.com', 08613000001123, 'female', 'cora.zhang', 'XuHui District');
 
-INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'tom.gao@hotmail.com', 08613000001124, 'fmale', 'tom.gao', 'HuangPu District');
+INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'tom.gao@hotmail.com', 08613000001124, 'female', 'tom.gao', 'HuangPu District');
--- src/main/java/com/boylegu/springboot_vue/entities/Persons.java
+++ src/main/java/com/boylegu/springboot_vue/entities/Persons.java
@@ -1,30 +1,37 @@
 package com.boylegu.springboot_vue.entities;
 
 import javax.persistence.*;
-import java.util.Date;
 import java.io.Serializable;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 
 @Entity
 @Table(name = "persons")
 public class Persons implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-
     private long id;
+
     @Column(name = "create_datetime")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date create_datetime;
+    /*
+       @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+       @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ", locale = "zh", timezone = "UTC")
+       @Type(type="datetime")
+    */
+    private String create_datetime;
+
     @Column(name = "username")
     private String username;
+
     @Column(name = "email")
     private String email;
+
     @Column(name = "phone")
     private String phone;
+
     @Column(name = "sex")
     private String sex;
+
     @Column(name = "zone")
     private String zone;
 
@@ -36,12 +43,12 @@ public class Persons implements Serializable {
         this.id = id;
     }
 
-    public Date getCreate_datetime() {
+    public String getCreate_datetime() {
         return create_datetime;
 
     }
 
-    public void setCreate_datetime(Date create_datetime) {
+    public void setCreate_datetime(String create_datetime) {
         this.create_datetime = create_datetime;
     }
 
