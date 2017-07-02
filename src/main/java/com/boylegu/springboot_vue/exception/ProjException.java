0227297706f6f2ff36fe41ce9b4a0ca53f36a6ac
顾鲍尔 <gubaoer@QXIT-PC-000087.local>
2017-07-02 22:27:01 +0800

    # add Exception handler

---
+++ src/main/java/com/boylegu/springboot_vue/exception/ProjException.java
@@ -0,0 +1,12 @@
+package com.boylegu.springboot_vue.exception;
+
+/**
+ * Created by gubaoer on 17/7/2.
+ */
+
+
+public class ProjException extends Exception {
+    public ProjException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/boylegu/springboot_vue/exception/ProjExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.boylegu.springboot_vue.exception;
+
+/**
+ * Created by gubaoer on 17/7/2.
+ */
+
+import com.boylegu.springboot_vue.dao.ErrorInfo;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ProjExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error");
+        return mav;
+    }
+
+    @ExceptionHandler(value = ProjException.class)
+    @ResponseBody
+    public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req, ProjException e) throws Exception {
+        ErrorInfo<String> r = new ErrorInfo<>();
+        r.setMessage(e.getMessage());
+        r.setCode(ErrorInfo.ERROR);
+        r.setData("Some Data");
+        r.setUrl(req.getRequestURL().toString());
+        return r;
+    }
+
+}
