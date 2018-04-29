40db87469c948e48cf78ccf948e7c495e153ae71
顾鲍尔 <gubaoer@QXIT-PC-000087.local>
2018-04-29 11:41:09 +0800

    #52 override build

---
+++ frontend/config/index.js
@@ -0,0 +1,24 @@
+// see http://vuejs-templates.github.io/webpack for documentation.
+var path = require('path')
+
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'statics',
+    assetsPublicPath: '/',
+    productionSourceMap: true,
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
+  dev: {
+    env: require('./dev.env'),
+    port: 8080,
+    autoOpenBrowser: true,
+    assetsSubDirectory: 'statics',
+    assetsPublicPath: '/',
+    cssSourceMap: false
+  }
+}
